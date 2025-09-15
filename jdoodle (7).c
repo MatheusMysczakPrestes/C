7e7c182bd3e63fc2c30ff6ad2b09d7a58db836c5
MatheusMysczakPrestes <tespres9@gmail.com>
2025-09-15 11:30:07 -0300

    Add files via upload

---
+++ jdoodle (7).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+ int main(){
+     float p1, p2, p3;
+     
+     printf("Informe o seu peso: ");
+     scanf("%f", &p1);
+     printf("Informe o peso do fulano: ");
+     scanf("%f", &p2);
+     printf("Informe o peso do ciclano: ");
+     scanf("%f", &p3);
+     
+     printf("O seu peso é %.2f , o peso do fulano é %.2f e o peso do ciclano é %.2f.", p1, p2, p3);
+
+    
+ }
