005f48269b2e754a741f4fbac457259ca351ea61
MatheusMysczakPrestes <tespres9@gmail.com>
2025-09-15 11:09:17 -0300

    Add files via upload

---
+++ jdoodle (4).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define NUM 75.7632489
+
+int main(){
+    printf("Número com 1 casa decimal: %.1f. \n", NUM);
+    printf("Número com 2 casa decimal: %.2f. \n", NUM);
+    printf("Número com 5 casa decimal: %.5f. \n", NUM);
+
+
+    
+ }
