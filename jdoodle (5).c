99387e8299b7e097dedb08b4cb0331e615178ef6
MatheusMysczakPrestes <tespres9@gmail.com>
2025-09-15 11:18:17 -0300

    Add files via upload

---
+++ jdoodle (5).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+ int main(){
+     float altura;
+     
+     printf("Digite a sua altura: ");
+     scanf("%f", &altura);
+     
+     printf("Sua altura com dua casas Décimais é: %.2f", altura);
+
+
+    
+ }
