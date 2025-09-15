b474c60869acbfa5454657ffbb7f65d8914d6c90
MatheusMysczakPrestes <tespres9@gmail.com>
2025-09-15 10:34:25 -0300

    Add files via upload

---
+++ jdoodle (1).c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+//#include<stdlib.h>
+
+int main(){
+    char cidade[20];
+    
+    printf("informe sua cidade natal: ");
+    scanf("%s", &cidade);
+    
+    printf("A sua cidade natal é %s.", cidade);
+}
